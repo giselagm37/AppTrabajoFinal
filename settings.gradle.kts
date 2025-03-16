40e6aee00517118add87eeb75001277cf7ad528b
giselagm37 <119113882+giselagm37@users.noreply.github.com>
2025-03-16 10:49:32 -0300

    Add files via upload
    Algunos cambios de color e imagen de inicio  y con el filtro funcionando

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,25 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
+        maven {
+            url = uri("https://jitpack.io")
+        }
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
     }
 }
 
-rootProject.name = "AppTrabajoFinal"
+rootProject.name = "AppChat"
 include(":app")
+
